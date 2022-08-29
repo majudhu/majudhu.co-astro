7e8d524ed168484b5462ccfaf980c6a29059f2f7
majudhu <majudhu.sup@gmail.com>
2022-08-30 01:27:52 +0500

    combine deno github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   base: process.env.BASE,
   integrations: [tailwind({ config: { applyBaseStyles: false } })],
-  output: "server",
-  adapter: deno(),
+
+  ...(process.env.DENO ? { output: "server", adapter: deno() } : {}),
 });
